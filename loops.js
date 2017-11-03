1b03926d67630607ded509e6b74bbecf9bff54dc
Ian Pavlovic <ian_pavlovic@msn.com>
2017-11-03 21:21:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,25 +1,17 @@
-function forLoop(array) {
+function forLoop(array){
   for (let i = 0; i < 25; i++){
-    if (i === 1){
-      array.push("I am 1 strange loop.")
-    }else {
-      array.push("I am ${i} strange loops.")
+    if (i === 1) {
+      array.unshift("I am 1 strange loop.")
+    }
+    else {
+      array.unshift("I am ${i} strange loops.")
     }
   }
   return array
 }
 function whileLoop(n){
-  while (n > 0){
-    console.log(--n)
+  while (n > 0) {
+  console.log(--n)
   }
-  return "done"
-}
-function maybeTrue(){
-  return Math.random() >= 0.5;
-}
-function doWhileLoop(array){
-  do {
-    array.pop();
-  }while (array.length > 0 && maybeTrue())
-  return array
+  return 'done'
 }
